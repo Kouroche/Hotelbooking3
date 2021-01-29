cf46053c256cb8ddc506e52174cb9db996e59894
Kouroche <73692398+Kouroche@users.noreply.github.com>
2021-01-29 08:57:59 +0100

    Startar Project

--- src/hotelbooking/Customer.java
+++ src/hotelbooking/Customer.java
@@ -1,14 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package hotelbooking;
 
-/**
- *
- * @author crush
- */
+
 public class Customer {
+    
+    System.out.println("Testar");
     
 }
